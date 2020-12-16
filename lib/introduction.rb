9e2c0177b0e0d16af0bc46062edc78a2fc5c78b0
Farah Malik <my.github.farahmalik82@hotmail.com>
2020-12-16 00:27:16 -0500

    commit tree 30be57542e6308d7accdd3510f53d829c19f3e75

---
+++ lib/introduction.rb
@@ -0,0 +1,18 @@
+# lib/introduction.rb
+
+def introduction
+end
+introduction
+# lib/introduction.rb
+
+def introduction(name)
+end 
+introduction(name)
+
+# lib/introduction.rb
+
+def introduction(name)
+    puts "Hi, my name is #{name}."
+  end 
+  introduction(name)
+  
